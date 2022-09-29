4d1d04ddb46047026005da13f946e6535b926058
Paul Nikolaevskiy <paulnikolaevsky@gmail.com>
2022-09-29 20:07:25 +0300

    The second trial of recursion.

--- gendiff/dict_diff.py
+++ gendiff/dict_diff.py
@@ -4,50 +4,59 @@ from gendiff.tools import get_files, get_first_file, get_second_file, is_equal_i
 def generate_diff(first_file=get_first_file(), second_file=get_second_file()):
     # if first_file is None and second_file is None:
     #     first_file, second_file = get_files()
+    def walk(first_file, second_file, current_lvl):
+        lvl_up = 1
+        lvl_down = 1
 
-    diff = list()
-    all_keys = sorted(list(
-        set(first_file.keys()) | set(second_file.keys())
-    ))
+        diff = list()
+        all_keys = sorted(list(
+            set(first_file.keys()) | set(second_file.keys())
+        ))
 
-    for key in all_keys:
-        if is_inner_nodes(first_file, second_file, key):
-            child = generate_diff(first_file[key], second_file[key])
-            diff.append({
-                'key': key,
-                'status': 'inner node',
-                'value': child
-            })
+        for key in all_keys:
+            if is_inner_nodes(first_file, second_file, key):
+                child = walk(first_file[key], second_file[key], current_lvl + 1)
+                diff.append({
+                    'key': key,
+                    'status': 'inner node',
+                    'lvl': current_lvl,
+                    'value': child
+                })
 
-        elif is_equal_items(first_file, second_file, key):
-            diff.append({
-                'key': key,
-                'status': 'equal',
-                'value': first_file[key]
-            })
+            elif is_equal_items(first_file, second_file, key):
+                diff.append({
+                    'key': key,
+                    'status': 'equal',
+                    'lvl': current_lvl,
+                    'value': first_file[key]
+                })
 
-        elif is_not_equal_items(first_file, second_file, key):
-            diff.append({
-                'key': key,
-                'operation': 'changed',
-                'value': (first_file[key], second_file[key])
-            })
+            elif is_not_equal_items(first_file, second_file, key):
+                diff.append({
+                    'key': key,
+                    'status': 'changed',
+                    'lvl': current_lvl,
+                    'value': (first_file[key], second_file[key])
+                })
 
-        elif key in first_file:
-            diff.append({
-                'key': key,
-                'status': 'deleted',
-                'value': first_file[key]
-            })
+            elif key in first_file:
+                diff.append({
+                    'key': key,
+                    'status': 'deleted',
+                    'lvl': current_lvl,
+                    'value': first_file[key]
+                })
 
-        elif key in second_file:
-            diff.append({
-                'key': key,
-                'status': 'added',
-                'value': second_file[key]
-            })
+            elif key in second_file:
+                diff.append({
+                    'key': key,
+                    'status': 'added',
+                    'lvl': current_lvl,
+                    'value': second_file[key]
+                })
 
-    return diff
+        return diff
+    return walk(first_file, second_file, 1)
 
 print(generate_diff())
 
